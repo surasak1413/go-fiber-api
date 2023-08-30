612bc7f947fc89781f2982af50d3c9fde4fecd26
surasak_po <surasak.po1413@gmail.com>
2023-08-30 21:25:23 +0700

    FEAT

--- main.go
+++ main.go
@@ -1,19 +1,17 @@
 package main
 
 import (
-	"loly/database"
-	"loly/repository"
 	"loly/routes"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
-	db, err := database.Connect()
-	if err != nil {
-		return
-	}
-	repository.NewStore(db)
+	// db, err := database.Connect()
+	// if err != nil {
+	// 	return
+	// }
+	// repository.NewStore(db)
 	app := fiber.New()
 	routes.Setup(app)
 
